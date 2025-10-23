c472515e167087811ca09fe73b55f2f13d599e3b
Ryan Tauro <23c35.rayan@sjec.ac.in>
2025-10-23 23:29:33 +0530

    feat: Endpoint to transcript audio

--- backend/routers/extension.py
+++ backend/routers/extension.py
@@ -1,13 +1,28 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, UploadFile, File, Request
+from ai.gemini import generateTranscript, randomMessage 
+from models import AudioTranscriptResp
 
 router = APIRouter(
   prefix="/extension",
 )
 
+@router.get("/")
+async def read_extension_root():
+  msg = randomMessage()
+  return {"message": msg}
+
 @router.post("/create")
 async def create_extension(data: dict):
-    #create a transcript based on data
-    #add to folder if required
-    #commit to database
-    #return ok if accepted
-    return {"message": "Extension created", "data": data}
+  #for transcript metadata
+  #create a transcript based on data
+  #add to folder if required
+  #commit to database
+  #return ok if accepted
+  return {"message": "Extension created", "data": data}
+
+@router.post("/audio")
+async def process_audio(request: Request):
+  audioFile: bytes = await request.body()
+  transcript = generateTranscript(audioFile)
+  #store transcript in database
+  return AudioTranscriptResp(transcript=transcript).model_dump()
